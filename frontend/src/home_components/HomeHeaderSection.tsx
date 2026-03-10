76ffa3765708ed8a09876a5936887b676f2067ae
YifeiXue111 <156253841+YifeiXue111@users.noreply.github.com>
2026-03-09 18:43:28 -0700

    removed white space

--- frontend/src/home_components/HomeHeaderSection.tsx
+++ frontend/src/home_components/HomeHeaderSection.tsx
@@ -5,7 +5,6 @@ import Header from "../../assets/Header.svg";
 const styles = StyleSheet.create({
   header: {
     width: "100%",
-    height: 150,
     justifyContent: "center",
     alignItems: "center",
   },
